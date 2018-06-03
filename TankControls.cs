ccea0de2c6a33dc5e2248df2c62a932c5d99ac42
Ozzyosboune <31003951+Ozzyosboune@users.noreply.github.com>
2018-06-03 14:30:01 -0700

    Add files via upload

---
+++ TankControls.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankControls : MonoBehaviour
+{
+    Transform tf;
+
+    public float rotationSpeed;
+
+	// Use this for initialization
+	void Start ()
+    {
+        tf = GetComponent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if(Input.GetKey(KeyCode.LeftArrow))
+        {
+            tf.Rotate(0, 0, rotationSpeed);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            tf.Rotate(0, 0, -rotationSpeed);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            tf.position -= tf.up;
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            tf.position += tf.up;
+        }
+    }
+}
